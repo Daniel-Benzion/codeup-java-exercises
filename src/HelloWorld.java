66799242209ae6f7030d0b69697916b2991fd3b6
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-01-20 11:29:13 -0500

    Syntax, types, and variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,29 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello, world!");
+
+        int myFavoriteNumber = 80;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is a string";
+        System.out.println(myString);
+
+        //myString = 'c';
+        //myString = 3.14159;
+        //long myNumber;
+        //myNumber = 3.14;
+        //myNumber = 123L;
+        //myNumber = 123;
+        //float myNumber = 3.14;
+        //myNumber = (float) 3.14;
+
+        /*
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+         */
+
+
+
     }
 }
