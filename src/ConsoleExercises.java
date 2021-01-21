db27a231d4521fc0fe48a6e5f8f09294c50a2579
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-01-21 11:25:54 -0500

    console exercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,29 @@
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class ConsoleExercises {
+
+    public static void main(String[] args) {
+
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately %.2f %n", pi);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer, please: ");
+        int userInt = scanner.nextInt();
+        System.out.printf("The integer you entered is: %d %n", userInt);
+        System.out.printf("The integer you entered times two is: %d %n", userInt * 2);
+
+        System.out.print("Please enter three words: ");
+        String uStr1 = scanner.next();
+        String uStr2 = scanner.next();
+        String uStr3 = scanner.next();
+        System.out.printf("Your words are: %n %s %n %s %n %s %n", uStr1, uStr2, uStr3);
+
+        scanner.nextLine();
+        System.out.print("Please enter a full sentence: ");
+        String uSent = scanner.nextLine();
+        System.out.printf("Your sentence is: %n %s", uSent);
+    }
+}
