758e49a2220b07aeaab07310516e3da528c32320
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-01-22 10:36:48 -0500

    Control Flow Exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -8,5 +8,21 @@ public class ControlFlowExercises {
             System.out.println(i);
             i++;
         }
+
+        System.out.println("-----");
+
+        i = 0;
+        do {
+            System.out.println(i);
+            i += 2;
+        } while (i <= 100);
+
+        System.out.println("-----");
+
+        i -= 2;
+        do {
+            System.out.println(i);
+            i -= 5;
+        } while (i >= -10);
     }
 }
