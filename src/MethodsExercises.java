7689331eacda60d47c60edd5a62816846cf3bed0
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-01-25 11:42:40 -0500

    Methods Exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -12,9 +12,19 @@ public class MethodsExercises {
         System.out.println(num1 * num2);
     }
 
+    public static void divide(int num1, int num2) {
+        if (num2 == 0) {
+            System.out.println("You can't divide by zero!");
+        } else {
+            System.out.println(num1 * 1.0 / num2);
+        }
+    }
+
     public static void main(String[] args) {
         add(800, 17);
         subtract(800, 17);
         multiply(800, 17);
+        divide(800, 17);
+        divide(800, 0);
     }
 }
