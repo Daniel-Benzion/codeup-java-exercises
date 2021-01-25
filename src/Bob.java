88deed35def32add62a70b3023a2c376994a1ca7
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-01-25 13:15:32 -0500

    Bob

--- src/Bob.java
+++ src/Bob.java
@@ -4,18 +4,24 @@ public class Bob {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Say something to Bob, ask a question, or say \"Goodbye\" to end the conversation:");
-		String input = scanner.nextLine();
+		System.out.println("Say something to Bob, ask a question, or say \"goodbye\" to end the conversation:\n");
 
-		if (input.endsWith("?")) {
-		    System.out.println("Sure.");
-		} else if (input.endsWith("!")) {
-		    System.out.println("Whoa, chill out!");
-		} else if (input.isEmpty()) {
-		    System.out.println("Fine. Be that way!");
-		} else {
-		    System.out.println("Whatever.");
+		while (true) {
+			String input = scanner.nextLine();
+			if (input.equalsIgnoreCase("goodbye")) {
+				System.out.println("Goodbye!");
+				break;
+			}else if (input.endsWith("?")) {
+				System.out.println("Sure.");
+			} else if (input.endsWith("!")) {
+				System.out.println("Whoa, chill out!");
+			} else if (input.isEmpty()) {
+				System.out.println("Fine. Be that way!");
+			} else {
+				System.out.println("Whatever.");
+			}
 		}
+
 
 	}
 }
