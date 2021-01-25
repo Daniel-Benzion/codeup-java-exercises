f6255a4e40b5f9e45aa5878639f17eafbd7fa9f9
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-01-25 15:24:26 -0500

    Methods Exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -44,6 +44,23 @@ public class MethodsExercises {
         }
     }
 
+    public static void factorial(int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter a number between %d and %d: ", min, max);
+        long input = Long.parseLong(scanner.nextLine());
+
+        System.out.printf("You entered %d. Do you wish to see the factorial? Y/N%n", input);
+        String conf = scanner.nextLine();
+        if (conf.equalsIgnoreCase("y")) {
+            System.out.printf("%d x", input);
+            for (long i = input - 1; i > 1; i--) {
+                System.out.printf(" %d x", i);
+                input *= i;
+            }
+            System.out.printf(" 1 = %d", input);
+        }
+    }
+
     public static void main(String[] args) {
         add(800, 17);
         subtract(800, 17);
@@ -52,5 +69,6 @@ public class MethodsExercises {
         divide(800, 0);
         mod(800, 17);
         getInteger(4, 16);
+        factorial(1, 20);
     }
 }
