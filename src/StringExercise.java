f0d4480245375574476f07c322c3b5b9adb0c89b
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-01-25 11:01:03 -0500

    String Exercises

---
+++ src/StringExercise.java
@@ -0,0 +1,10 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+
+        String str1 = "We don't need no education\nWe don't need no thought control";
+        System.out.println(str1);
+        String str2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(str2);
+    }
+}
