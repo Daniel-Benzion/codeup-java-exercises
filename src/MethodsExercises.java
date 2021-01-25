d6bf7c33e34bf444f30b2abaf57cd4dfabb4f045
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-01-25 11:39:27 -0500

    Methods Exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -8,8 +8,13 @@ public class MethodsExercises {
         System.out.println(num1 - num2);
     }
 
+    public static void multiply(int num1, int num2) {
+        System.out.println(num1 * num2);
+    }
+
     public static void main(String[] args) {
         add(800, 17);
         subtract(800, 17);
+        multiply(800, 17);
     }
 }
