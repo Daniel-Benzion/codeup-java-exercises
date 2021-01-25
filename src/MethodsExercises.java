009e50196402a6ce2562c1350b04587ed2c201e0
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-01-25 11:38:20 -0500

    Methods Exercises

---
+++ src/MethodsExercises.java
@@ -0,0 +1,10 @@
+public class MethodsExercises {
+
+    public static void add(int num1, int num2) {
+        System.out.println(num1 + num2);
+    }
+
+    public static void main(String[] args) {
+        add(800, 17);
+    }
+}
