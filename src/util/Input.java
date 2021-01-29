9d76ed8570fe5a630915fc956f3db7a098adff2d
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-01-29 11:45:57 -0500

    OOP Exercises

---
+++ src/util/Input.java
@@ -0,0 +1,53 @@
+package util;
+
+import java.util.*;
+
+public class Input {
+
+    private Scanner scanner;
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getString() {
+        System.out.println("Enter a string:");
+        return scanner.nextLine();
+    }
+
+    public boolean yesNo() {
+        System.out.println("Yes or no?");
+        return scanner.nextLine().equalsIgnoreCase("yes");
+    }
+
+    public int getInt(int min, int max) {
+        while (true) {
+            System.out.println("Enter a number: ");
+            int input = Integer.parseInt(scanner.nextLine());
+            if (input <= max && input >= min) {
+                return input;
+            }
+        }
+    }
+
+    public int getInt() {
+        System.out.println("Enter an integer:");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public double getDouble(double min, double max) {
+        while (true) {
+            System.out.println("Enter a number: ");
+            double input = Double.parseDouble(scanner.nextLine());
+            if (input <= max && input >= min) {
+                return input;
+            }
+        }
+
+    }
+
+    public double getDouble() {
+        System.out.println("Enter a decimal number:");
+        return Double.parseDouble(scanner.nextLine());
+    }
+}
