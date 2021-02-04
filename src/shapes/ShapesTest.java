1a6d495c2577e23e9df9624e57a4c63c03332bc1
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-02-04 10:51:20 -0500

    Interfaces and Abstract Classes exercises

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,6 +3,7 @@ package shapes;
 public class ShapesTest {
 
     public static void main(String[] args) {
+        /*
         Rectangle box1 = new Rectangle(5, 4);
         System.out.println("Area: " + box1.getArea());
         System.out.println("Perimeter: " + box1.getPerimeter());
@@ -11,5 +12,21 @@ public class ShapesTest {
         Rectangle box2 = new Square(5);
         System.out.println("Area: " + box2.getArea());
         System.out.println("Perimeter: " + box2.getPerimeter());
+
+         */
+
+        Measurable myShape = new Rectangle(3.0, 5.0);
+
+        System.out.println("It's a rectangle: ");
+
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
+
+        System.out.println("Now it's a square:");
+
+        myShape = new Square(3.0);
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
+
     }
 }
