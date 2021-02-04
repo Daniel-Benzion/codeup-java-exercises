f368661eabd26d5df47b9a565ad1979604896195
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-02-04 10:44:29 -0500

    Interfaces and Abstract Classes exercises

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,7 +1,8 @@
 package shapes;
 
-public class Square extends Rectangle {
+public class Square extends Quadrilateral implements Measurable {
 
+    /*
     private final int side;
 
     public Square(int side) {
@@ -18,5 +19,31 @@ public class Square extends Rectangle {
     @Override
     public int getPerimeter() {
         return side * 4;
+    }
+
+     */
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public void setLength(double side) {
+        this.length = side;
+        this.width = side;
+    }
+
+    @Override
+    public void setWidth(double side) {
+        this.setLength(side);
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (length + width);
     }
 }
